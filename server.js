ccf4b2b5eef0e1a26e6b141505f8d7f62f6aedee
Christopher Campbell <campbefs@gmail.com>
2021-04-18 17:42:33 -0700

    added wildcard

--- server.js
+++ server.js
@@ -58,6 +58,11 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 
 app.get('/api/animals', (req, res) => {
   // res.send('Hello!');
@@ -67,6 +72,16 @@ app.get('/api/animals', (req, res) => {
   }
   console.log(req.query);
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  // res.json(result);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 // app.listen(3001, () => {
